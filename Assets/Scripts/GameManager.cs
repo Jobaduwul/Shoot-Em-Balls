fa16d18f6fb3c9f7e4597e5e82761b300df6eb1a
Kazi Jawadul Islam Jishan <kazijawadulislam123@gmail.com>
2023-07-24 02:43:55 +0600

    initial commit with some fixes to be made

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.5f;
+    public TextMeshProUGUI scoreText;
+    private int score;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleScreen.gameObject.SetActive(false);
+        isGameActive = true; //need to ensure game is active before spawning can occur
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //coroutine to spawn targets
+    IEnumerator SpawnTarget()
+    {
+        //condition to spawn targets
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    //method to update score
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        if(score < 0)
+        {
+            score = 0;
+        }
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    //method will be added to list of OnCick of restart button
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
